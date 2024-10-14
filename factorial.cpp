b8ee736970b4d8e1ea05edf265ab6a90b4d1a8c3
starl1ght0 <st4rl1ght0s@gmail.com>
2024-10-14 20:53:15 +0700

    addFactorial.cpp

---
+++ factorial.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+// N! = N * (N - 1)!
+
+int Fact(int N) {
+    if (N == 0) {
+        return 0;
+    } 
+    if (N == 1) {
+        return 1;
+    }
+
+    return N * Fact(N - 1);
+}
+
+int main() {
+    int N;
+    cout << "Enter number: ";
+    cin >> N;
+    cout << "Factorial N = " << Fact(N) << endl;
+
+}
